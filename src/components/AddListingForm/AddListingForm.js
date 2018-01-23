f578fdac16a2c406866d20acde6b0164b0b68951
Sydney Flak <sflak@porter-kresge-apts-57-98.resnet.ucsc.edu>
2018-01-23 15:02:27 -0800

    In the middle of making the input component

--- src/components/AddListingForm/AddListingForm.js
+++ src/components/AddListingForm/AddListingForm.js
@@ -1,24 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import FileUploader from 'react-firebase-file-uploader';
 
 import Button from '../UI/Button/Button';
+// import ImageUploader from '../../containers/ImageUploader/ImageUploader'
 
 import classes from './AddListingForm.css';
 
-const addListingForm = (props) => {
 
-    return (
-        <div className={classes.AddListingForm}>
-            <p>Add a listing to your inventory</p>
-            <form className={classes.Form}>
-                <input type="text" placeholder="Title" />
-                <textarea placeholder="Description" rows="15"></textarea>
-                <Button label="Create"/>
-            </form>
+class AddListingForm extends Component {
+    state = {
+        item: {
+            itemName: {
+                elementType: 'input',
+                elementConfig: {
+                  type: 'text',
+                  placeholder: 'Item Name'
+              },
+              value: ''
+            },
+            desc: {
+                elementType: 'textarea',
+                elementConfig: {
+                  type: 'text',
+                  placeholder: 'Item Description'
+              },
+              value: ''
+            },
+            imageURL: ''
+        },
+        isUploading: false,
+        progress: 0,
+    }
 
-        </div>
+    handleUploadStart = () => this.setState({isUploading: true, progress: 0});
+    handleProgress = (progress) => this.setState({progress});
+    handleUploadError = (error) => {
+        this.setState({isUploading: false});
+        console.error(error);
+    }
+    handleUploadSuccess = (filename) => {
+        this.setState({avatar: filename, progress: 100, isUploading: false});
+        firebase.storage().ref('images').child(filename).getDownloadURL().then(url => this.setState({imageURL: url}));
+    };
 
-    );
+    addListingHandler = () => {
+        const listing = {};
+    }
 
-};
+	render () {
 
-export default addListingForm;
+        /*Display the image if one has been uploaded*/
+        let image = null
+        if (this.state.imageURL){
+            image = (
+                <div className={classes.Image}>
+                    <img src={this.state.imageURL} alt=''/>
+                </div>
+            )
+        }
+		return (
+            <div className={classes.AddListingForm}>
+                <form className={classes.Form}>
+                    {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+                    {/* {this.state.imageURL && <img className={classes.Image} src={this.state.imageURL} alt=''/> } */}
+                    {image}
+                    <FileUploader
+                            accept="image/*"
+                            name="item"
+                            randomizeFilename
+                            storageRef={firebase.storage().ref('images')}
+                            onUploadStart={this.handleUploadStart}
+                            onUploadError={this.handleUploadError}
+                            onUploadSuccess={this.handleUploadSuccess}
+                            onProgress={this.handleProgress}
+                    />
+                    <input type="text" placeholder="Title" />
+                    <textarea placeholder="Description" rows="15"></textarea>
+                    <Button label="Create" clicked={this.addListingHandler}/>
+                </form>
+
+            </div>
+
+        );
+
+
+    }
+
+}
+
+export default AddListingForm;
