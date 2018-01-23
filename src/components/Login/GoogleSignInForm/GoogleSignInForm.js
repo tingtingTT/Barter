6e3fca148988efa26a8453bbe17e789b51761abc
Kyle Batross <kmcraebatross@gmail.com>
2018-01-23 11:19:41 -0800

    Added Functional Google Sign in button.

--- src/components/Login/GoogleSignInForm/GoogleSignInForm.js
+++ src/components/Login/GoogleSignInForm/GoogleSignInForm.js
@@ -1,8 +1,37 @@
+/* global gapi */
 import React, { Component } from 'react';
 import axios from 'axios';
 import classes from './GoogleSignInForm.css';
 
+var googleUser = {};
+
 class GoogleSignInForm extends React.Component {
+
+  constructor(props){
+      super(props);
+      this.onSignIn = this.onSignIn.bind(this)
+  }
+
+  componentDidMount() {
+      console.log('this mounted')
+      gapi.signin2.render('my-signin2', {
+          'scope': 'profile email',
+          'width': '380',
+          'height': 50,
+          'longtitle': true,
+          'theme': 'dark',
+          'onsuccess': this.onSignIn,
+      });
+  }
+
+
+  onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 
   render() {
     return (
@@ -12,32 +41,9 @@ class GoogleSignInForm extends React.Component {
             <p className={classes.lead}>
               Log in with your google account.
             </p>
-            <div className={classes.formGroup}>
-             <a className={classes.google} href="/login/google">
-               <svg
-                 className={classes.icon}
-                 width="30"
-                 height="30"
-                 viewBox="0 0 30 30"
-                 xmlns="http://www.w3.org/2000/svg"
-               >
-                 <path
-                   d={
-                     'M30 13h-4V9h-2v4h-4v2h4v4h2v-4h4m-15 2s-2-1.15-2-2c0 0-.5-1.828 1-3 ' +
-                     '1.537-1.2 3-3.035 3-5 0-2.336-1.046-5-3-6h3l2.387-1H10C5.835 0 2 3.345 2 7c0 ' +
-                     '3.735 2.85 6.56 7.086 6.56.295 0 .58-.006.86-.025-.273.526-.47 1.12-.47 1.735 ' +
-                     '0 1.037.817 2.042 1.523 2.73H9c-5.16 0-9 2.593-9 6 0 3.355 4.87 6 10.03 6 5.882 ' +
-                     '0 9.97-3 9.97-7 0-2.69-2.545-4.264-5-6zm-4-4c-2.395 0-5.587-2.857-6-6C4.587 ' +
-                     '3.856 6.607.93 9 1c2.394.07 4.603 2.908 5.017 6.052C14.43 10.195 13 13 11 ' +
-                     '13zm-1 15c-3.566 0-7-1.29-7-4 0-2.658 3.434-5.038 7-5 .832.01 2 0 2 0 1 0 ' +
-                     '2.88.88 4 2 1 1 1 2.674 1 3 0 3-1.986 4-7 4z'
-                   }
-                 />
-               </svg>
-               <span>Log in with Google</span>
-             </a>
+            {/* data-onsuccess={this.onSignIn} */}
+            <div id='my-signin2' className={classes.formGroup}>
            </div>
-         </div>
          <strong className={classes.lineThrough}>OR</strong>
           <form method="post">
             <div className={classes.formGroup}>
@@ -69,6 +75,7 @@ class GoogleSignInForm extends React.Component {
               </button>
             </div>
           </form>
+       </div>
        </div>
      )
 
