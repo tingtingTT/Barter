8e9b5cfebd06cd2d5d029aa8f47ba5d4b1f80ed4
Sydney Flak <sflak@eduroam-169-233-210-119.ucsc.edu>
2018-01-24 13:16:55 -0800

    commit before pull

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import classes from './Input.css'
+
+const input = (props) => {
+    let inputElement = null;
+
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}/>;
+            break;
+        case ('textarea'):
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                rows="10"
+                value={props.value}/>;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option =>(
+                        <option value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input className={classes.InputElement} {...props}/>;
+
+    }
+
+    return(
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+
+
+};
+
+export default input;
