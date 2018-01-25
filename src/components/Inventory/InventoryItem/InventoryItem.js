59489cba68ccb96ebd39c03a286cebb6118ff3bb
Sydney Flak <sflak@porter-kresge-apts-59-73.resnet.ucsc.edu>
2018-01-25 13:05:49 -0800

    make images smaller in inventory

--- src/components/Inventory/InventoryItem/InventoryItem.js
+++ src/components/Inventory/InventoryItem/InventoryItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import classes from './InventoryItem.css'
+
 const inventoryItem = (props) => (
 
-    <div>
+    <div className={classes.Item}>
         <img src={props.img} />
         <p>{props.name}</p>
         <p>{props.desc}</p>
