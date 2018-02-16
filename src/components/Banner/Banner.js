32b98dfb0515c2fd925f3a6f7c1f8816be51bf8d
Sydney Flak <sflak@porter-kresge-apts-59-83.resnet.ucsc.edu>
2018-02-16 10:04:12 -0800

    Add "logged in" state to banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,20 +5,37 @@ import BigButton from '../UI/BigButton/BigButton';
 import classes from './Banner.css'
  
  
-class Banner extends Component {
-  render() {
-    return (
-        <div>
-            <div className={classes.Banner}>
-                <div className={classes.joinText}>
-                    <h1 className={classes.title}> Join Barter</h1>
-                    <h3 className={classes.font}>Join the community that's changing how we trade goods.</h3>
-                </div>
-                <BigButton text="Sign-Up" />    
+const Banner = (props) => {
 
+    const LoggedIn = false;
+
+    let banner = (
+        
+        <div className={classes.Banner}>
+            <div className={classes.joinText}>
+                <h1 className={classes.title}> Join Barter</h1>
+                <h3 className={classes.font}>Join the community that's changing how we trade goods.</h3>
             </div>
+            <BigButton text="Sign-Up" />    
+
         </div>
     );
-  }
+
+    if (LoggedIn) {
+        banner = (
+            <div className={classes.Banner}>
+                <div className={classes.logo}>
+                    <img src="https://i.imgur.com/7AgYdWZ.png" alt="Barter Logo" />
+                </div>
+            </div>
+        );
+    }
+  
+    return (
+        <div>
+            {banner}
+        </div>
+    );
+  
 }
 export default Banner;
