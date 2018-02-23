7d4a3c8ae9e7716ffbbca76c8f80bf96475c2868
Sydney Flak <sflak@porter-kresge-apts-57-186.resnet.ucsc.edu>
2018-02-22 17:02:00 -0800

    click handler and Query parameters set up for item Details

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,52 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import firebase from 'firebase';
 
 import classes from './ItemDetails.css';
 
-const itemDetails = ( props ) => {
+class ItemDetails extends Component {
     //TODO: Get item from DB using props.itemID
-
-    //Dummy Item
-    const item = {
-        title: 'DVD Collection',
-        desc: 'This is my extensive DVD collection. I have a multitude of genres. They are very rare. wow.',
-        img: 'https://i.ytimg.com/vi/0S-gteTLcko/maxresdefault.jpg',
-        zipCode: 92004,
-        owner: 'PennyMonster38'
+    state = {
+        item: {}
+    
     }
 
+    componentWillMount () {
+        console.log('item');
+        const query = new URLSearchParams(this.props.location.search);
+        const item = {};
+        for (let param of query.entries()){
+            
+            item[param[0]] = param[1];
+        }
+
+        this.setState({item: item});
+
+        
+    }
+
+    // //Dummy Item
+    
     // Once this is implemented using Props, need to change everything that gets info from Items
-    return (
-        <div className = {classes.ItemDetails}>
-            <div className = {classes.row}>
-                <div className = {classes.col1of2}>
-                    
-                    <h1 className={classes.title}>{item.title}</h1>
-                    <img src={item.img} className={classes.image}/>
-                    <div className={classes.ownerDetails}>
-                        {/* this is a placeholder for now, it should be the userIcon */}
-                        <div className={classes.userIcon}></div>
-                        <p className = {classes.owner}>{item.owner}</p>
-                        <p className = {classes.owner}>[rating]</p>
+    render() {
+        const item = {
+            title: 'DVD Collection',
+            desc: 'This is my extensive DVD collection. I have a multitude of genres. They are very rare. wow.',
+            img: 'https://i.ytimg.com/vi/0S-gteTLcko/maxresdefault.jpg',
+            zipCode: 92004,
+            owner: 'PennyMonster38'
+        }
+        console.log('this.state.item');
+        console.log(this.state.item);
+    
+        return (
+
+            <div className = {classes.ItemDetails}>
+                <div className = {classes.row}>
+                    <div className = {classes.col1of2}>
+                        
+                        <h1 className={classes.title}>{this.state.item.name}</h1>
+                        <img src={this.state.item.img} className={classes.image}/>
+                        <div className={classes.ownerDetails}>
+                            {/* this is a placeholder for now, it should be the userIcon */}
+                            <div className={classes.userIcon}></div>
+                            <p className = {classes.owner}>{item.owner}</p>
+                            <p className = {classes.owner}>[rating]</p>
+                        </div>
+                        
                     </div>
-                    
+    
+                    <div className = {classes.col1of2}>
+                        {/*pass in item.zipCode as props to the map*/}
+                        MAP
+                    </div>
                 </div>
-
-                <div className = {classes.col1of2}>
-                    {/*pass in item.zipCode as props to the map*/}
-                    MAP
+                <div className = {classes.row}>
+                    <div className = {classes.description}>
+                        <p classes = {classes.descText}>{this.state.item.desc}</p>
+                    </div>
                 </div>
+    
             </div>
-            <div className = {classes.row}>
-                <div className = {classes.description}>
-                    <p classes = {classes.descText}>{item.desc}</p>
-                </div>
-            </div>
-
-        </div>
-    );
+        );
+    }
+   
 
     
 }
 
-export default itemDetails;
+export default ItemDetails;
