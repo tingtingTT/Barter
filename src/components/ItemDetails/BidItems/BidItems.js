01965a56a523e430b41bf5964460382a41e1aaef
Tingting Wang <MyLaptop@Tingtings-Air.hsd1.ca.comcast.net>
2018-03-03 20:12:53 -0800

    show multiple bids

--- src/components/ItemDetails/BidItems/BidItems.js
+++ src/components/ItemDetails/BidItems/BidItems.js
@@ -9,14 +9,44 @@ import { withRouter } from 'react-router-dom';
 
 const bidItems = (props) => {
 
+    // check if the owner already exists in the target object 
+    function isInTarget(targetObj, owner){
+        console.log(targetObj);
+        console.log(owner);
+        for (var i=0; i< targetObj.length; i++){
+            if(targetObj[i].owner === owner){
+                return true;
+            }
+        }
+        return false;
+    }
 
     let bidItems = null
     var that = this;
     if(props.bidItems){
+        var bidItemsComb = []
+        props.bidItems.forEach(function(item){
+            var searchResult = isInTarget(bidItemsComb, item.owner);
+            var newItem = item
+            if(searchResult === false){
+                newItem['isNew'] = true;
+                bidItemsComb.push(newItem);
+            }
+            else{
+                console.log(bidItemsComb);
+                console.log(item.title);
+                newItem['isNew'] = false;
+                bidItemsComb.push(newItem);
+            }
+        });
+
+        console.log(bidItemsComb);
+
         bidItems = (
-            props.bidItems.map((item, index) => (
+            bidItemsComb.map((item, index) => (
                 <BidItem key={index}
                     owner={item.owner}
+                    isNew = {item.isNew}
                     title={item.title}
                     zipcode={item.zipcode}
                     itemOwner={props.itemOwner}
