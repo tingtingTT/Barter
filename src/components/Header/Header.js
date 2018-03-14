d1712dd0f1f6aa522b2cd947f049f550128f9d1c
Kyle Batross <kmcraebatross@gmail.com>
2018-03-14 14:18:44 -0700

    added barter extra home navlink. Hello world.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import NavigationItems from './NavigationItems/NavigationItems';
 import classes from './Header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = (props) => (
     <header className={classes.Header}>
         <div className={classes.Logo}>
-            BARTER
+          <p className={classes.barter}>  <NavLink to='/' exact> Barter </NavLink></p>
         </div>
         <div>
             <nav>
