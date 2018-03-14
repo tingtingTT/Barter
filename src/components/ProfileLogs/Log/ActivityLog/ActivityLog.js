86e5e8a198d4023974a955ced4671e0842b7cab5
Kyle Batross <kmcraebatross@gmail.com>
2018-03-14 13:15:42 -0700

    conditionally render 'with'

--- src/components/ProfileLogs/Log/ActivityLog/ActivityLog.js
+++ src/components/ProfileLogs/Log/ActivityLog/ActivityLog.js
@@ -4,13 +4,15 @@ import React from 'react';
 import classes from './ActivityLog.css'
 
 const activityLog = (props) => {
+
+
     return(
         <div className={classes.ActivityLog}>
             <div className={classes.textnBorder}>
                 {props.user}
                 <div className={classes.smallText}>{props.action1}</div>
                 {props.item1}
-                <div className={classes.smallText}>with</div>
+                <div className={classes.smallText}>{props.item2 === '' ? '' : 'with'}</div>
                 {props.item2}
                 <div className={classes.date}>{props.date}</div>
             </div>
